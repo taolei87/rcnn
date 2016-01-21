4358c2094884292b6fec76ee03d20eaa4c10fd93
Tao Lei <taolei@rosetta4.csail.mit.edu>
2016-01-20 20:44:25 -0500

    added a missing file

---
+++ code/utils/__init__.py
@@ -0,0 +1,21 @@
+
+import sys
+import gzip
+
+import numpy as np
+
+def say(s, stream=sys.stderr):
+    stream.write("{}".format(s))
+    stream.flush()
+
+def load_embedding_iterator(path):
+    file_open = gzip.open if path.endswith(".gz") else open
+    with file_open(path) as fin:
+        for line in fin:
+            line = line.strip()
+            if line:
+                parts = line.split()
+                word = parts[0]
+                vals = np.array([ float(x) for x in parts[1:] ])
+                yield word, vals
+
